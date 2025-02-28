b59c85376958801359ff3d550508795c84f49308
Akshitha Sriyanjith <akshitha.s@prime.lk>
2025-02-28 10:17:36 +0530

    first commit

---
+++ POS Front End System/POS Front End System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POS_Front_End_System.Startup))]
+namespace POS_Front_End_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
